b07c157962c4130f3d43b12b7004a0500c899c69
zilmukhamedoff <askhat.zilmukhamedov@gmail.com>
2024-03-26 12:10:40 -0600

    add Game

---
+++ MathGameCLI/Models/Game.cs
@@ -0,0 +1,9 @@
+﻿namespace MathGameCLI.Models
+{
+    internal class Game
+    {
+        public DateTime Date { get; set; }
+        public int Score { get; set; }
+        public GameType Type { get; set; }
+    }
+}
