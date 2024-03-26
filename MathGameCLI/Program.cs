eb107b8eccd009f9a6b3635e8f8e13f1bf5d5070
zilmukhamedoff <askhat.zilmukhamedov@gmail.com>
2024-03-26 13:43:35 -0600

    update Program

--- MathGameCLI/Program.cs
+++ MathGameCLI/Program.cs
@@ -4,7 +4,10 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var menu = new Menu();
+            var date = DateTime.UtcNow;
+            var name = Helpers.GetName();
+            menu.ShowMenu(name, date);
         }
     }
 }
